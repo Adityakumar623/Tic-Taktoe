cad0c45a4f932c49ab62d3d625455a720c77fc8b
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2023-01-08 16:10:04 +0530

    hhistory part 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,47 @@ import { calculateWinner } from "./helpers";
 
 const App=()=> {
 
-  const[board,setBoard]=useState(Array(9).fill(null));
-  const[isXNext,setIsXNext]=useState(false);
+  const [history,setHistory]=useState([{board :Array(9).fill(null) , isXNext:true},
+  ]);
+ const[currentMove,setCurrentMove]=useState(0);
   
-  const winner=calculateWinner(board);
+  const current=history[currentMove];
   
-
-  const message=winner?`winner is ${winner}`
-  :`Next player is ${isXNext?'X':'0'}`;
+const winner=calculateWinner(current.board);
+  const message=winner
+  ?`winner is ${winner}`
+  :`Next player is ${current.isXNext?'X':'0'}`;
   
   const handleSquareClick=(position)=>{
   
-  if(board[position] || winner ){
+  if(current.board[position] || winner ){
     return;
-  }
+  }   
   
   
-    setBoard((prev)=>{
-    return prev.map((square,pos) => {
+    setHistory((prev)=>{
+const last=prev[prev.length-1];
+
+
+    const newBoard= last.board.map((square,pos) => {
       if(pos===position){
-        return isXNext ?'X':'0';
+        return last.isXNext ?'X':'0';
       }
       return square;
     });
+
+    return prev.concat({board:newBoard,isXNext:!last.isXNext})
     });
     
-    setIsXNext((prev)=>!prev);
+   // setIsXNext((prev)=>!prev);
+   setCurrentMove(prev=>prev+1);
   }
 
  return(
   <div className="app">
     <h1>Tic Tak Toe</h1>
     <h2>{message}</h2>
-    <Board board={board} handleSquareClick={handleSquareClick} />
+    <Board board={current.board} handleSquareClick={handleSquareClick} />
   </div>
  );
 
