8e1fb47a7503f6b7c69784446439a74c01f93810
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2023-01-08 15:41:48 +0530

    calculate winner and lift up space

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,34 +1,11 @@
-import React ,{useState} from 'react'
+import React  from 'react'
 import Square from './Square'
 
 
 
 
-const Board = () => {
-const[board,setBoard]=useState(Array(9).fill(null));
-const[isXNext,setIsXNext]=useState(false);
+const Board = ({board,handleSquareClick}) => {
 
-
-
-
-const handleSquareClick=(position)=>{
-
-if(board[position ]){
-  return;
-}
-
-
-  setBoard((prev)=>{
-  return prev.map((square,pos) => {
-    if(pos===position){
-      return isXNext ?'X':'0';
-    }
-    return square;
-  });
-  });
-  
-  setIsXNext((prev)=>!prev);
-}
 const renderSquare=(position)=>{
   return<Square value={board[position]} 
   onClick={()=>handleSquareClick(position)
